ab33dc323162ef64319b11555588e0e124ca54aa
Michael Giesler <michael.giesler1@icloud.com>
2024-12-12 15:30:41 -0700

    updated env.js and .env with the correct connection strings

--- client/src/env.js
+++ client/src/env.js
@@ -1,6 +1,6 @@
-export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
-export const useSockets = false
-export const domain = 'dev-s6n62lvv6zbfoman.us.auth0.com'
-export const clientId = 'NafJKRvSf1xchjf3sAEl9q1k9I3WwH3T'
-export const audience = 'dev-s6n62lvv6zbfoman.us.auth0.com'
+export const dev = window.location.origin.includes('localhost');
+export const baseURL = dev ? 'http://localhost:3000' : '';
+export const useSockets = false;
+export const domain = process.env.VUE_APP_AUTH_DOMAIN || 'dev-s6n62lvv6zbfoman.us.auth0.com';
+export const clientId = process.env.VUE_APP_AUTH_CLIENT_ID || 'NafJKRvSf1xchjf3sAEl9q1k9I3WwH3T';
+export const audience = process.env.VUE_APP_AUTH_AUDIENCE || 'dev-s6n62lvv6zbfoman.us.auth0.com';
