a67c1bff1d8e6abc155211f085d67181d02e283d
Michael Giesler <michael.giesler1@icloud.com>
2024-12-12 14:41:21 -0700

    updated client and server side env's

--- client/src/env.js
+++ client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'https://auth.michaelgiesler.com'
+export const clientId = 'NafJKRvSf1xchjf3sAEl9q1k9I3WwH3T'
+export const audience = 'dev-s6n62lvv6zbfoman.us.auth0.com'
