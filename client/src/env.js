36da4cd6e76e84459f12a1f63ff40f596ddef10e
Michael Giesler <michael.giesler1@icloud.com>
2024-12-12 15:41:41 -0700

    updated package management features

--- client/src/env.js
+++ client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost');
 export const baseURL = dev ? 'http://localhost:3000' : '';
 export const useSockets = false;
-export const domain = process.env.VUE_APP_AUTH_DOMAIN || 'dev-s6n62lvv6zbfoman.us.auth0.com';
-export const clientId = process.env.VUE_APP_AUTH_CLIENT_ID || 'NafJKRvSf1xchjf3sAEl9q1k9I3WwH3T';
-export const audience = process.env.VUE_APP_AUTH_AUDIENCE || 'dev-s6n62lvv6zbfoman.us.auth0.com';
+export const domain = import.meta.env.VITE_AUTH_DOMAIN || 'dev-s6n62lvv6zbfoman.us.auth0.com';
+export const clientId = import.meta.env.VITE_AUTH_CLIENT_ID || 'NafJKRvSf1xchjf3sAEl9q1k9I3WwH3T';
+export const audience = import.meta.env.VITE_AUTH_AUDIENCE || 'dev-s6n62lvv6zbfoman.us.auth0.com';
