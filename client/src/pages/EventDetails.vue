1402e344464d9889edbf435b417a29da4b0bf6a4
Michael Giesler <michael.giesler1@icloud.com>
2024-12-12 21:55:08 -0700

    created event details page with ticket functionality and status indicators

---
+++ client/src/pages/EventDetails.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="event-details">
+    <div class="event-header">
+      <img :src="event.coverImg" :alt="event.name">
+      <div class="event-info">
+        <h1>{{ event.name }}</h1>
+        <span :class="{'canceled': event.isCanceled}">
+          {{ event.isCanceled ? 'CANCELED' : event.ticketCount >= event.capacity ? 'SOLD OUT' : 'AVAILABLE' }}
+        </span>
+      </div>
+    </div>
+    <button
+      @click="purchaseTicket"
+      :disabled="event.isCanceled || event.ticketCount >= event.capacity"
+    >
+      Get Ticket
+    </button>
+  </div>
+</template>
