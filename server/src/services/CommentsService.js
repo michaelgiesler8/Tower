cbce01a46576337e19cc3bf2db1e27a0d8e41c71
Michael Giesler <michael.giesler1@icloud.com>
2024-12-14 17:30:55 -0700

    added proper imports to comments service (server side) and made small changes

--- server/src/services/CommentsService.js
+++ server/src/services/CommentsService.js
@@ -1,16 +1,19 @@
-import { dbContext } from '../db/DbContext';
+import { dbContext } from "../db/DbContext.js";
+import { BadRequest, Forbidden } from '../utils/Errors.js'
 
-class CommentService {
+class CommentsService {
+  async getEventComments(eventId) {
+    const comments = await dbContext.Comments.find({ eventId }).populate('creator', 'name picture');
+    return comments;
+  }
+
   async createComment(commentData) {
     const comment = await dbContext.Comments.create(commentData);
     await comment.populate('creator');
     return comment;
   }
 
-  async getCommentsByEvent(eventId) {
-    const comments = await dbContext.Comments.find({ eventId }).populate('creator', 'name picture');
-    return comments;
-  }
+
 
   async deleteComment(commentId, userId) {
     const comment = await dbContext.Comments.findById(commentId);
@@ -18,11 +21,11 @@ class CommentService {
       throw new Error('Comment not found');
     }
     if (comment.creatorId.toString() !== userId) {
-      throw new Error('Unauthorized');
+      throw new Error('Not yours to delete!');
     }
     await dbContext.Comments.findByIdAndDelete(commentId);
     return { message: 'Comment Successfully Deleted' };
   }
 }
 
-export const commentService = new CommentService();
+export const commentsService = new CommentsService();
