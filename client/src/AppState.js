d04c6cbc7d35a607a0eb553837a4ccc219a6a8b8
Michael Giesler <michael.giesler1@icloud.com>
2024-12-15 15:32:42 -0700

    reworked app state and added various states

--- client/src/AppState.js
+++ client/src/AppState.js
@@ -1,10 +1,20 @@
 import { reactive } from 'vue'
 
-// NOTE AppState is a reactive object to contain app level data
+
 export const AppState = reactive({
   /**@type {import('@bcwdev/auth0provider-client').Identity} */
   identity: null,
-  /** @type {import('./models/Account.js').Account} user info from the database*/
-  account: null
+  /** @type {import('./models/Account.js').Account} */
+  account: null,
+   /** @type {import('./models/TowerEvent.js').TowerEvent[]} */
+  towerEvents: [],
+   /** @type {import('./models/TowerEvent.js').TowerEvent|null} */
+  activeEvent: null,
+   /** @type {import('./models/Ticket.js').Ticket[]} */
+  tickets: [],
+   /** @type {import('./models/TowerEvent.js').TowerEvent[]} */
+  myEvents: [],
+   /** @type {import('./models/Comment.js').Comment} */
+   comments: []
 })
 
