c823438eb1f317df263c23947dc415ac74f71a84
Michael Giesler <michael.giesler1@icloud.com>
2024-12-14 18:41:11 -0700

    setup ticket management endpoints

---
+++ server/src/controllers/TicketController.js
@@ -0,0 +1,45 @@
+import { Auth0Provider } from "@bcwdev/auth0provider";
+import BaseController from "../utils/BaseController";
+import { ticketService } from "../services/TicketsService";
+
+export class TicketsController extends BaseController {
+  constructor() {
+    super('api/tickets')
+    this.router
+      .get('/:eventId, this.getEventTickets)
+        .use(Auth0Provider.getAuthorizedUserInfo)
+        .post('', this.createTicket)
+        .delete('/:id', this.removeTicket)
+  }
+
+  async getEventTickets(req, res, next) {
+    try {
+      const eventId = req.params.eventId
+      const tickets = await ticketService.getEventTickets(eventId)
+      return res.send(tickets)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async createTicket(req, res, next) {
+    try {
+      req.body.accountId = req.userInfo.id
+      const ticket = await ticketService.createTicket(req.body)
+      return res.send(ticket)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async removeTicket(req, res, next) {
+    try {
+      const userId = req.userInfo.id
+      const ticketId = req.params.id
+      const message = await ticketService.removeTicket(ticketId, userId)
+      return res.send(message)
+    } catch (error) {
+      next(error)
+    }
+  }
+}
