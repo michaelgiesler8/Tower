70ac687215d8e208e10778cc22e3b530bef36cd9
Michael Giesler <michael.giesler1@icloud.com>
2024-12-15 14:26:12 -0700

    added loading states to ticket service methods

--- client/src/services/TicketsService.js
+++ client/src/services/TicketsService.js
@@ -1,22 +1,47 @@
 import { AppState } from "../AppState.js"
-import { Ticket } from "../models/Ticket.js"
 import { api } from "./AxiosService.js"
+import Pop from "../utils/Pop.js"
 
 class TicketsService {
   async getEventTickets(eventId) {
-    const res = await api.get(`api/tickets/${eventId}`)
-    AppState.tickets = res.data.map(ticket => new Ticket(ticket))
+    AppState.loading = true
+    try {
+      const res = await api.get(`api/tickets/${eventId}`)
+      AppState.tickets = res.data
+    } catch (error) {
+      Pop.error(error)
+    }
+    finally {
+      AppState.loading = false
+    }
   }
 
   async createTicket(eventId) {
-    const res = await api.post('api/tickets', { eventId })
-    AppState.tickets.push(new Ticket(res.data))
-    return res.data
+    AppState.loading = true
+    try {
+      const res = await api.post('api/tickets', { eventId })
+      AppState.tickets.push(res.data)
+      return res.data
+    } catch (error) {
+      Pop.error(error)
+    }
+    finally {
+      AppState.loading = false
+    }
   }
 
+
   async removeTicket(ticketId) {
-    await api.delete(`api/tickets/${ticketId}`)
-    AppState.tickets = AppState.tickets.filter(t => t.id != ticketId)
+    AppState.loading = true
+    try {
+      await api.delete(`api/tickets/${ticketId}`)
+      AppState.tickets = AppState.tickets.filter(t => t.id != ticketId)
+    } catch (error) {
+      Pop.error(error)
+    }
+    finally {
+      AppState.loading = false
+    }
   }
 }
 
