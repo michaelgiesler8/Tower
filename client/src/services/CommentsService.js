ee2e49529b1b42b1fc94945325ab84838d605c3b
Michael Giesler <michael.giesler1@icloud.com>
2024-12-15 14:33:16 -0700

    added loading states to comments service

--- client/src/services/CommentsService.js
+++ client/src/services/CommentsService.js
@@ -1,20 +1,45 @@
-import { api } from './AxiosService'
-import { AppState } from '../AppState'
+import { api } from "./AxiosService.js"
+import { AppState } from "../AppState.js"
+import Pop from "../utils/Pop.js"
 
 class CommentsService {
   async getEventComments(eventId) {
-    const res = await api.get(`api/comments/${eventId}`)
-    AppState.comments = res.data
+    AppState.loading = true
+    try {
+      const res = await api.get(`api/events/${eventId}/comments`)
+      AppState.comments = res.data
+    } catch (error) {
+      Pop.error(error)
+    }
+    finally {
+      AppState.loading = false
+    }
   }
-
   async createComment(commentData) {
-    const res = await api.post('api/comments', commentData)
-    AppState.comments.push(res.data)
+    AppState.loading = true
+    try {
+      const res = await api.post('api/comments', commentData)
+      AppState.comments.push(res.data)
+      return res.data
+    } catch (error) {
+      Pop.error(error)
+    }
+    finally {
+      AppState.loading = false
+    }
   }
 
   async deleteComment(commentId) {
-    await api.delete(`api/comments/${commentId}`)
-    AppState.comments = AppState.comments.filter(c => c.id !== commentId)
+    AppState.loading = true 
+    try {
+      await api.delete(`api/comments/${commentId}`)
+      AppState.comments = AppState.comments.filter(c => c.id !== commentId)
+    } catch (error) {
+      Pop.error(error)
+    }
+    finally {
+      AppState.loading = false
+    }
   }
 }
 
