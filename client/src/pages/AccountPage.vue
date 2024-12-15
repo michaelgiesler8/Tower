28d0f8605c82f09e132243e4eeb26deb2b673bf3
Michael Giesler <michael.giesler1@icloud.com>
2024-12-15 12:25:48 -0700

    added attended events and hosted events display to the account dashboard

--- client/src/pages/AccountPage.vue
+++ client/src/pages/AccountPage.vue
@@ -1,26 +1,83 @@
-<script setup>
-import { computed } from 'vue';
-import { AppState } from '../AppState.js';
-
-const account = computed(() => AppState.account)
-
-</script>
-
 <template>
-  <div class="about text-center">
-    <div v-if="account">
-      <h1>Welcome {{ account.name }}</h1>
-      <img class="rounded" :src="account.picture" alt="" />
-      <p>{{ account.email }}</p>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12">
+        <h1>Welcome {{  account.name }}</h1>
+        <img :src="account.picture" alt="" class="rounded" />
+      </div>
     </div>
-    <div v-else>
-      <h1>Loading... <i class="mdi mdi-loading mdi-spin"></i></h1>
+
+    <div class="row mt-4">
+      <div class="col-12">
+        <h2>My Tickets</h2>
+        <div class="row">
+          <div v-for="event in myTickets" :key="event.id" class="col-md-4 mb-3">
+            <TowerEventCard :event="event" />
+            <button @click="removeTicket(event.ticketId)" class="btn btn-danger mt-2">Remove Ticket</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="row mt-4">
+      <div class="col-12">
+        <h2>Events I'm Hosting</h2>
+        <div class="row">
+          <div v-for="event in myEvents" :key="event.id" class="col-md-4 mb-3">
+            <TowerEventCard :event="event" />
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
-<style scoped lang="scss">
-img {
-  max-width: 100px;
+<script>
+import { computed, onMounted } from 'vue'
+import { AppState } from '../AppState'
+import { ticketsService } from '../services/TicketsService'
+import Pop from '../utils/Pop'
+
+export default {
+  setup() {
+    onMounted(() => {
+      getMyTickets()
+      getMyEvents()
+    })
+
+    async function getMyTickets() {
+      try {
+        await ticketsService.getMyTickets()
+      } catch (error) {
+        Pop.error(error)
+      }
+    }
+
+    async function getMyEvents() {
+      try {
+        await eventsService.getMyEvents()
+      } catch (error) {
+        Pop.error(error)
+      }
+    }
+
+    return {
+      account: computed(() => AppState.account),
+      myTickets: computed(() => AppState.myTickets),
+      myEvents: computed(() => AppState.myEvents),
+
+      async removeTicket(ticketId) {
+        try {
+          if (await Pop.confirm('Remove this ticket?')) {
+            await ticketsService.removeTicket(ticketId)
+            Pop.success('Ticket removed')
+          }
+        } catch (error) {
+          Pop.error(error)
+        }
+      }
+    }
+  }
 }
-</style>
+</script>
+
