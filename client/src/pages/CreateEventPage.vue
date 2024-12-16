aac8709dcae1c1c20762ecb65f67d11e79776724
Michael Giesler <michael.giesler1@icloud.com>
2024-12-15 18:07:50 -0700

    integrated create event page with event form

---
+++ client/src/pages/CreateEventPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <h1>Create New Event</h1>
+    <EventForm />
+  </div>
+</template>
+
+<script>
+import EventForm from '../components/EventForm.vue'
+
+export default {
+  components: { EventForm }
+}
+</script>
+
+<style scoped lang="scss">
+.container {
+  padding: 2rem;
+
+  h1 {
+    text-align: center;
+    margin-bottom: 2rem;
+  }
+}
+</style>
